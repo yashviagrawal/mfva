95793ac3c5056d3ee169605e20e5612af4547760
Yashvi Agrawal <74981992+yashviagrawal@users.noreply.github.com>
2024-08-26 23:25:02 +0530

    Update: androidx imports

--- app/src/main/java/com/appknox/mfva/ApiRequestsActivity.java
+++ app/src/main/java/com/appknox/mfva/ApiRequestsActivity.java
@@ -1,10 +1,10 @@
 package com.appknox.mfva;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
